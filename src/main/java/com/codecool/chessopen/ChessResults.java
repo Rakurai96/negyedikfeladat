447511f5413d24f70de02d04579f670c6753e9ca
Rakurai96 <balbianello96@gmail.com>
2021-03-24 17:58:52 +0100

    chess

--- src/main/java/com/codecool/chessopen/ChessResults.java
+++ src/main/java/com/codecool/chessopen/ChessResults.java
@@ -1,9 +1,7 @@
 package com.codecool.chessopen;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.nio.Buffer;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
@@ -13,8 +11,47 @@ import java.util.Map;
 public class ChessResults {
 
     public List<String> getCompetitorsNamesFromFile(String fileName){
-        //TODO your code comes here
+        if(fileName != null) {
+            ClassLoader classLoader = getClass().getClassLoader();
+            File file = new File(classLoader.getResource(fileName).getFile());
+            List<String> name = new ArrayList<>();
+            List<Integer> result = new ArrayList<>();
+            try {
+                BufferedReader br = new BufferedReader(new FileReader(file));
+                String line;
+                while ((line = br.readLine()) != null) {
+                    String[] sp = line.split(",");
+                    name.add(sp[0]);
+                    int sum = 0;
+                    sum += Integer.parseInt(sp[1]);
+                    sum += Integer.parseInt(sp[2]);
+                    sum += Integer.parseInt(sp[3]);
+                    sum += Integer.parseInt(sp[4]);
+                    sum += Integer.parseInt(sp[5]);
+                    result.add(sum);
+                }
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return Sort(name, result);
+        }
         return null;
     }
-
+    private List<String> Sort(List<String> name, List<Integer> result) {
+        for(int i = 0; i < result.size(); i++) {
+            for(int j = i + 1; j < result.size(); j++) {
+                if(result.get(i) < result.get(j)){
+                    int swapRes = result.get(i);
+                    result.set(i, result.get(j));
+                    result.set(j, swapRes);
+                    String swapName = name.get(i);
+                    name.set(i, name.get(j));
+                    name.set(j, swapName);
+                }
+            }
+        }
+        return name;
+    }
 }
